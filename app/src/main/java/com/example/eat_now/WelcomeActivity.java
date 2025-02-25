09a6f2c68c881fdce4333100ea2708b6eaef487f
Hazerakteritu <hazerakteritu@gmail.com>
2025-02-26 02:21:12 +0600

    Creating Registration Page

--- app/src/main/java/com/example/eat_now/WelcomeActivity.java
+++ app/src/main/java/com/example/eat_now/WelcomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.eat_now;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,20 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        }); */
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
     }
 }
