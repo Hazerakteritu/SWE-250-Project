c539b284bceeb5da16349a84af616a8660acd4c1
Hazerakteritu <hazerakteritu@gmail.com>
2025-03-01 22:06:05 +0600

    home horizontal scrolling restaurant

--- app/src/main/java/com/example/eat_now/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/eat_now/ui/home/HomeFragment.java
@@ -9,13 +9,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.eat_now.R;
+import com.example.eat_now.adapters.HomeHorAdapter;
 import com.example.eat_now.databinding.FragmentHomeBinding;
+import com.example.eat_now.models.HomeHorModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    //private FragmentHomeBinding binding;
+    RecyclerView homehorrec;
+    List<HomeHorModel> homeHorModelList;
+    HomeHorAdapter homeHorAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +32,22 @@ public class HomeFragment extends Fragment {
         //binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        homehorrec = root.findViewById(R.id.home_hor_rec);
+
+        homeHorModelList = new ArrayList<>();
+
+        homeHorModelList.add(new HomeHorModel(R.drawable.tastytreatlogo, "Tasty Treat"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.tastytreatlogo2, "Tasty Treat"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.tastytreatlogo2, "Tasty Treat"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.tastytreatlogo2, "Tasty Treat"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.tastytreatlogo2, "Tasty Treat"));
+
+        homeHorAdapter = new HomeHorAdapter(getActivity(), homeHorModelList);
+        homehorrec.setAdapter(homeHorAdapter);
+
+        homehorrec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        homehorrec.setHasFixedSize(true);
+        homehorrec.setNestedScrollingEnabled(false);
         return root;
     }
 
